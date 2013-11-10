6e4b355499fb6433b90cfd6fe203a590d7c7e291
adaniele87 <andrew_daniele@hotmail.com>
2013-11-10 16:17:53 -0500

    changed move behaviour

--- moving.cpp
+++ moving.cpp
@@ -1,4 +1,5 @@
 #include <Windows.h>
+#include <iostream>
 #include <cstdarg>
 #include <conio.h>
 #include "console.h"
@@ -10,22 +11,27 @@ void testCollision(Object&, int = console.getCols()-1, int = console.getRows()-1
 void moveEnemyClose(Object&, Object&);
 bool tooClose(Object&, Object&);
 bool contact(Object&, Object&);
+bool foundOat(Object&, Object&);
 
 int main() 
 {
     bool done = false;
+    char gameOver[40] = "You Died! Score: ";
     int Ticks = GetTickCount();
     srand(Ticks);
     int rMove =1;
     int r = console.getRows()-1;
     int c = console.getCols()-1;
+    int score = 0;
 
     Object mover(c/2,r/2);
     Object mover2(c,0);
     Object mover3(0,r);
     Object mover4(c,r);
-    Object me(0,0,3);
+    Object me(0,0);
+    Object Oat(rand()%c, rand()%r);
 
+    int slowDown = 0;
     int i = 20;
     while(!done)
     {
@@ -36,6 +42,9 @@ int main()
         mover3.draw(character);
         mover4.draw(character);
         me.draw(character);
+        Oat.draw(oat);
+
+        me.getInput();
 
         if (i==20)
         {
@@ -43,55 +52,66 @@ int main()
             rMove = rand()%5;
             i=0;
         }
-        me.getInput();
-
-        switch (rMove)
+        if (slowDown == 2)
         {
-        default:
-            if (!tooClose(mover, me)) mover.moveUp();
-            if (!tooClose(mover2, me)) mover2.moveRight();
-            if (!tooClose(mover3, me)) mover3.moveLeft();
-            if (!tooClose(mover4, me)) mover4.moveDown();
-            break;
-        case 2:
-            if (!tooClose(mover, me)) mover.moveRight();
-            if (!tooClose(mover2, me)) mover2.moveUp();
-            if (!tooClose(mover3, me)) mover3.moveRight();
-            if (!tooClose(mover4, me)) mover4.moveLeft();
-            break;
-        case 3:
-            if (!tooClose(mover, me)) mover.moveDown();
-            if (!tooClose(mover2, me)) mover2.moveLeft();
-            if (!tooClose(mover3, me)) mover3.moveDown();
-            if (!tooClose(mover4, me)) mover4.moveUp();
-            break;
-        case 4:
-            if (!tooClose(mover, me)) mover.moveLeft();
-            if (!tooClose(mover2, me)) mover2.moveDown();
-            if (!tooClose(mover3, me)) mover3.moveUp();
-            if (!tooClose(mover4, me)) mover4.moveRight();
-            break;
+            switch (rMove)
+            {
+            default:
+                if (!tooClose(mover, me)) mover.moveUp();
+                if (!tooClose(mover2, me)) mover2.moveRight();
+                if (!tooClose(mover3, me)) mover3.moveLeft();
+                if (!tooClose(mover4, me)) mover4.moveDown();
+                break;
+            case 2:
+                if (!tooClose(mover, me)) mover.moveRight();
+                if (!tooClose(mover2, me)) mover2.moveUp();
+                if (!tooClose(mover3, me)) mover3.moveRight();
+                if (!tooClose(mover4, me)) mover4.moveLeft();
+                break;
+            case 3:
+                if (!tooClose(mover, me)) mover.moveDown();
+                if (!tooClose(mover2, me)) mover2.moveLeft();
+                if (!tooClose(mover3, me)) mover3.moveDown();
+                if (!tooClose(mover4, me)) mover4.moveUp();
+                break;
+            case 4:
+                if (!tooClose(mover, me)) mover.moveLeft();
+                if (!tooClose(mover2, me)) mover2.moveDown();
+                if (!tooClose(mover3, me)) mover3.moveUp();
+                if (!tooClose(mover4, me)) mover4.moveRight();
+                break;
+            }
+            slowDown = 0;
         }
         testCollision(mover);
         testCollision(mover2);
         testCollision(mover3);
         testCollision(mover4);
         testCollision(me);
-
+        
         done = contact(me, mover);
         if(!done) done = contact(me, mover2);
         if(!done) done = contact(me, mover3);
         if(!done) done = contact(me, mover4);
         
-        i++;
-        
-        if (1000/20 > GetTickCount()-Ticks)
+        if (contact(me, Oat))
         {
-            Sleep(1000/20-(GetTickCount()-Ticks));
+            Oat.x(rand()%c);
+            Oat.y(rand()%r);
+            score++;
+        }
+
+        i++;
+        slowDown++;
+        
+        if (1000/60 > GetTickCount()-Ticks)
+        {
+            Sleep(1000/60-(GetTickCount()-Ticks));
         }
     }
     console.clear();
-    console.display("YOU DIED!", r/2, c/2/4);
+    console.display("YOU DIED! score: ", r/2, c/2/4);
+    std::cout << score;
     while(true);
 }
 
